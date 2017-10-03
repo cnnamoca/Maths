4d811ff8090e73f7260c96cee904d0a74a2e6f36
Carlo <carlo.namoca@gmail.com>
2017-10-03 13:32:14 -0700

    Initial Commit

---
+++ Maths/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Maths
+//
+//  Created by Carlo Namoca on 2017-10-03.
+//  Copyright © 2017 Carlo Namoca. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
